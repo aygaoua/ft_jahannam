ea1f954a61986b0be3740e1e8d0c083ac0f014e6
aygaoua <zgaoua72@gmail.com>
2025-04-24 09:41:53 +0100

    continue ...

--- my-app/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-
-function Header(props) {
-    return (
-      <h1>Header {props.text}</h1>
-    )
-}
-
-export default Header;
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,40 @@
+import React from 'react';
 import TicTacToe from './components/tictactoe/TicTacToe';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProjectedRoute';
+
+function logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  return <TicTacToe />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+            <ProtectedRoute>
+            <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={logout()} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/tictactoe" element={<TicTacToe />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
